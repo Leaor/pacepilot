57bb26f04405f67ae3a9421f273533b0f9863ac1
QuoteFLO <hello@quoteflo.com>
2026-06-23 01:05:33 -0400

    Initial Commit

---
+++ pacepilot/pacepilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  pacepilotApp.swift
+//  pacepilot
+//
+//  Created by Michael on 2026-06-23.
+//
+
+import SwiftUI
+
+@main
+struct pacepilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
